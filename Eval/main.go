cec272d98c6c6b30fe418d8396aa45afc9ef6064
wcastillo <williams.cruz.pro@gmail.com>
2021-01-15 16:55:25 +0100

    partie 2 du projet question 2

--- Eval/main.go
+++ Eval/main.go
@@ -8,36 +8,37 @@ import (
 
 func main() {
 	
-	var tark []Task
-
-
-	func list(rw http.ResponseWriter, _ *http.Request) {
-		io.WriteString(w, "Hello from a HandleFunc #1!\n")
-	}
-	
-	func done(rw http.ResponseWriter, _ *http.Request) {
-		io.WriteString(w, "Hello from a HandleFunc #2!\n")
-	}
-
-	func add(rw http.ResponseWriter, _ *http.Request) {
-                io.WriteString(w, "Hello from a HandleFunc #3!\n")
-        }
-
-
 	http.HandleFunc("/", list)
-	http.HandleFunc("/done", done)
-	http.HandleFunc("/add", add)
-	http.ListenAndServe(":8080", nil)
+        http.HandleFunc("/done", done)
+        http.HandleFunc("/add", add)
+        http.ListenAndServe(":8080", nil)
+	
+
+		
+
+		
 }
-
-
 
 type Task struct {
         Description string
         Done        bool
 }
 
-func modifSlice(sl []string, idx int){
-	
+type List struct {
+	ID   string 
+	Task string
+
 }
+
+var task []Task {}
+func list(rw http.ResponseWriter, _ *http.Request){
+	
+}	
+
+
+
+
+
+	
+
 
